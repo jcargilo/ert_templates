7afaed5be747ec4fda03cbfb88581b7b831546f8
Jeremy Cargilo <jcargilo@gmail.com>
2022-02-04 15:38:30 -0500

    Configure webpack/npm

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  content: [],
+  content: [
+    './resources/js/**/*.{html,js}',
+    './resources/views/**/*.blade.php',
+    './packages/takeoffdesigngroup/cms/resources/views/**/*.blade.php',
+  ],
   theme: {
     extend: {},
   },
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,17 +1,16 @@
 const mix = require('laravel-mix');
+const path = require('path')
+require('laravel-mix-merge-manifest')
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
- */
+mix
+  .mergeManifest()
+  .sass('resources/sass/app.scss', 'public/css')
+  .options({
+    processCssUrls: false,
+    postCss: [require('autoprefixer'), require('tailwindcss')('./tailwind.config.js')],
+  })
+  .version()
 
-// mix.js('resources/js/app.js', 'public/js')
-//     .postCss('resources/css/app.css', 'public/css', [
-//         //
-//     ]);
+if (!mix.inProduction()) {
+  mix.sourceMaps()
+}

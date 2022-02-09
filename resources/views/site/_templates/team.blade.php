ada85754cef572bf179243285aee2f15a94397ac
Jeremy Cargilo <jcargilo@gmail.com>
2022-02-09 15:45:37 -0500

    Restyle to match LinkedIn layout

--- resources/views/site/_templates/team.blade.php
+++ resources/views/site/_templates/team.blade.php
@@ -1,33 +1,7 @@
 @extends ('site.index')
 
-@php
-    $gap = "lg:gap-x-20";
-    $size = "h-40 w-40 xl:w-56 xl:h-56";
-    if ($name === 'Virtual Family Office') {
-        $grid = "lg:grid-cols-6";
-        $gap = "lg:gap-x-16";
-        $size = "h-40 w-40";
-    } else {
-        switch (count($team)) {
-            case 1:
-                $grid = "lg:grid-cols-1";
-                break;
-            case 2:
-                $grid = "lg:grid-cols-2";
-                break;
-            case 3:
-            case 5:
-            case 6:
-                $grid = "lg:grid-cols-3";
-                break;
-            default:
-                $grid = "lg:grid-cols-4";
-        }
-    }
-@endphp
-
 @section('page_content')
-    <section id="row-1" class="content-block relative pt-10 pb-10">
+    <section id="row-1" class=" pt-10 pb-10">
         <div class="max-w-screen-xl space-y-12 text-center lg:mx-auto px-4 xl:px-0">
             <div class="space-y-5 sm:mx-auto sm:max-w-xl sm:space-y-4 lg:max-w-5xl">
                 <h2 class="text-3xl font-extrabold tracking-tight sm:text-4xl">{{ $name }}</h2>
@@ -35,24 +9,34 @@
             </div>
             
             @if (count($team) > 0)
-                <ul role="list" class="space-y-16 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 {{ $grid }} {{ $gap }}">
-                    @foreach ($team as $advisor)
-                    <li>
-                        <div class="space-y-4">
-                            <img class="mx-auto rounded-full {{ $size }} object-cover" src="{{ $advisor['PhotoURL'] }}" alt="">
+                <div class="max-w-4xl mx-auto">
+                    <ul role="list" class="grid grid-cols-4 gap-6">
+                        @foreach ($team as $advisor)
+                        <li class="w-full flex mb-0">
+                            <div class="w-full relative bg-white border border-gray-200 rounded-lg p-3 hover:border-gray-300 hover:shadow-md">
+                                <div class="absolute top-0 left-0 z-0 w-full h-[62px] bg-blue-500 rounded-t-lg"></div>
 
-                            <div class="space-y-2">
-                                <div class="text-lg leading-6 font-medium space-y-1">
-                                    <h3>{{ $advisor['Name'] }}</h3>
-                                    <p class="text-blue-500">{{ $advisor['ShortBio'] }}</p>
+                                <div class="h-full flex flex-col relative z-10 space-y-4">
+                                    <img class="mx-auto h-32 w-32 rounded-full object-cover" src="{{ $advisor['PhotoURL'] }}" alt="">
+
+                                    <div class="flex-1">
+                                        <h3 class="font-medium text-lg leading-6">{{ $advisor['Name'] }}</h3>
+                                        <p class="text-[14px] text-gray-500">{{ $advisor['ShortBio'] }}</p>
+                                    </div>
+
+                                    <div>
+                                        <button class="w-full px-6 py-1 border border-blue-500 rounded-2xl hover:bg-blue-50 hover:ring-1 hover:ring-blue-500 focus:bg-blue-50 focus:ring-1 focus:ring-blue-500 active:bg-blue-100 active:ring-1 active:ring-blue-900" aria-label="">
+                                            <span class="font-medium">Bio</span>
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </li>
-                    @endforeach
-                </ul>
+                        </li>
+                        @endforeach
+                    </ul>
+                </div>
             @else
-                <p class="text-xl text-gray-500">Ornare sagittis, suspendisse in hendrerit quis. Sed dui aliquet lectus sit pretium egestas vel mattis neque.</p>
+                <p class="text-xl text-gray-500">No members have been added to the {{ $name }} team.</p>
             @endif
         </div>
     </section>

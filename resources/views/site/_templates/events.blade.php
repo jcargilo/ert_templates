1e61bac7f2ecca65cde84f593eace6b3095f8f00
Jeremy Cargilo <jcargilo@gmail.com>
2022-03-22 23:50:02 -0400

    Update events placeholder

--- resources/views/site/_templates/events.blade.php
+++ resources/views/site/_templates/events.blade.php
@@ -1,10 +1,69 @@
-<div class="bg-fixed" style="background-image: url(./images/event-bg.jpg)">
-    <div class="px-6 pt-10 pb-14 bg-black/40">
-        <div class="max-w-screen-xl lg:mx-auto">
-            <div class="pb-8 text-center">
-                <span class="text-2xl font-semibold text-white">Upcoming Events</span>
-            </div>
-            <div class="w-2/3 m-auto"><a href="https://lindsay.oureliteexperience.com/MTPW0005" target="_blank"><img src="./images/maximizing-advanced-tax-planning.jpg"></a></div>
+<div class="bg-gray-200 px-6 pt-8 pb-12">
+  <div class="pb-6 text-center">
+    <h2>Upcoming Events</h2>
+  </div>
+
+  <div class="mx-auto block max-w-4xl space-y-6 gap-x-8 lg:space-y-0 lg:flex lg:items-stretch">
+    <div class="w-full flex-1 pb-2 md:pb-0">
+      <div class="flex flex-col overflow-hidden rounded-lg shadow-lg">
+        <div class="flex-shrink-0">
+          <a class="block group bg-gray-500" href="https://lindsay.oureliteexperience.com/UEPW0001" target="_blank">
+            <img class="w-full object-cover group-hover:opacity-75" src="/images/UnderstandingEstatePlanning_OpenGraph.jpg" alt="" />
+          </a>
         </div>
+
+        <div class="flex flex-1 flex-col border-t bg-white px-6 pt-6 pb-2">
+          <div class="flex-1 pb-4">
+            <p class="text-sm font-semibold text-gray-900 md:text-base">
+              <time datetime="2022-04-12 11:00">Tuesday, April 12th | 11:00 am EDT</time>
+            </p>
+            <a
+              href="https://lindsay.oureliteexperience.com/UEPW0001"
+              class="text-lg font-semibold text-link hover:text-link-hover sm:text-xl active:text-link-active"
+              target="_blank"
+            >
+              Understanding Estate Planning
+            </a>
+            <p class="my-2 text-base leading-tight text-gray-900">
+              Don't leave your loved ones in the dark regarding your wishes. Join us for this important webinar.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
+
+    <div class="flex flex-1 flex-col space-y-6 lg:space-y-4">
+      <div class="flex-1 rounded-lg bg-white px-6 pb-6 pt-4">
+        <p class="text-sm font-semibold text-gray-900 md:text-base">
+          <time datetime="2022-04-29 12:00">Friday, April 29th | 12:00 pm EDT</time>
+        </p>
+        <a
+          href="https://lindsay.oureliteexperience.com/UEPW0001"
+          class="text-lg font-semibold text-link hover:text-link-hover sm:text-xl active:text-link-active"
+          target="_blank"
+        >
+          Maximizing Advanced Tax Planning
+        </a>
+        <p class="my-2 text-base leading-tight text-gray-900">
+          A little planning and the right financial advisor can go a long way when it comes to paying the lowest taxes possible.
+        </p>
+      </div>
+
+      <div class="flex-1 rounded-lg bg-white px-6 pb-6 pt-4">
+        <p class="text-sm font-semibold text-gray-900 md:text-base">
+          <time datetime="2022-05-18 10:00">Wednesday, May 18th | 10:00 am EDT</time>
+        </p>
+        <a
+          href="https://lindsay.oureliteexperience.com/UEPW0001"
+          class="text-lg font-semibold text-link hover:text-link-hover sm:text-xl active:text-link-active"
+          target="_blank"
+        >
+          Placeholder Event Title
+        </a>
+        <p class="my-2 text-base leading-tight text-gray-900">
+          Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo.
+        </p>
+      </div>
+    </div>
+  </div>
 </div>
